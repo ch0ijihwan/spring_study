9873148208dbc6daf96827b820f5101361a96af5
cjh8746 <cjh87467@gmail.com>
2022-02-08 20:44:25 +0900

    style : 컨벤션에 따르수정

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -27,7 +27,7 @@ public class MemberService {
         return memberRepository.findAll();
     }
 
-    public Optional<Member> findOne(Long memberId){
+    public Optional<Member> findOne(Long memberId) {
         return memberRepository.findById(memberId);
     }
 }
