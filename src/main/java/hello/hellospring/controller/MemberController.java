8e2ca6935f00a92e4e7cc6624adb6d4f04a18d83
cjh8746 <cjh87467@gmail.com>
2022-02-14 21:06:13 +0900

    style : 오타 수정

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -37,7 +37,7 @@ public class MemberController {
     @GetMapping("/members")
     public String list(Model model) {
         List<Member> members = memberService.findMembers();
-        model.addAttribute("members",members);
-        return  "members/memberList";
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
