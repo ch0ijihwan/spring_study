8b092a69e13d5d68e348bb1e8c8a0ffdc4a62cbc
cjh8746 <cjh87467@gmail.com>
2022-02-25 09:59:31 +0900

    refactor : 코드 가독성을 위한 메서드 분리

--- 스프링 기본 강의/core/src/main/java/helo/core/AppConfig.java
+++ 스프링 기본 강의/core/src/main/java/helo/core/AppConfig.java
@@ -1,6 +1,8 @@
 package helo.core;
 
+import helo.core.discount.DiscountPolicy;
 import helo.core.discount.FixDiscountPolicy;
+import helo.core.member.MemberRepository;
 import helo.core.member.MemberService;
 import helo.core.member.MemberServiceImpl;
 import helo.core.member.MemoryMemberRepository;
@@ -9,10 +11,18 @@ import helo.core.order.OrderServiceIml;
 
 public class AppConfig {
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceIml(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceIml(memberRepository(), discountPolicy());
+    }
+
+    private DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
