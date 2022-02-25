c638f6f0b6252fcfec0ffbc2472efbbdf53bbbd2
cjh8746 <cjh87467@gmail.com>
2022-02-25 12:33:38 +0900

    style : 불필요한 줄 띄움 제거

--- 스프링 기본 강의/core/src/main/java/helo/core/MemberApp.java
+++ 스프링 기본 강의/core/src/main/java/helo/core/MemberApp.java
@@ -10,7 +10,6 @@ public class MemberApp {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
         MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
-
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
         Member findMember = memberService.findMember(1L);
