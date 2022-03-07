de2a717a768c826e96100da3b76956810c678947
cjh8746 <cjh87467@gmail.com>
2022-03-07 16:57:13 +0900

    test : 테스트코드 작성

---
+++ 스프링 기본 강의/core/src/test/java/hello/core/order/OrderServiceImlTest.java
@@ -0,0 +1,17 @@
+package hello.core.order;
+
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemoryMemberRepository;
+import org.junit.jupiter.api.Test;
+
+class OrderServiceImlTest {
+    @Test
+    void createOrder() {
+        MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+        memberRepository.save(new Member(1L, "name", Grade.VIP));
+        OrderServiceIml orderService = new OrderServiceIml(memberRepository, new FixDiscountPolicy());
+        Order order = orderService.createOrder(1L, "itemA", 100000);
+    }
+}
