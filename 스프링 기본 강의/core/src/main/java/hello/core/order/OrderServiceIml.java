018f39df7f51611602091087d4524f30d210bee5
cjh8746 <cjh87467@gmail.com>
2022-03-10 17:22:17 +0900

    refactor : @Qualifier 를 사용한 중복빈 문제 해결 예시

--- 스프링 기본 강의/core/src/main/java/hello/core/order/OrderServiceIml.java
+++ 스프링 기본 강의/core/src/main/java/hello/core/order/OrderServiceIml.java
@@ -3,6 +3,7 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,7 +11,7 @@ public class OrderServiceIml implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    public OrderServiceIml(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public OrderServiceIml(MemberRepository memberRepository, @Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
