7865d10d25463731075fd0a6974e151cb905e9ec
cjh8746 <cjh87467@gmail.com>
2022-03-31 16:28:51 +0900

    feat : 로깅 간단히 알아보기 예제 코드 작성

---
+++ 스프링 mvc1 1편/springmvc/src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,23 @@
+package hello.springmvc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class LogTestController {
+//    private final Logger log = LoggerFactory.getLogger(getClass()); --> 롬복 @ Slf4j로 사용가능
+
+    @RequestMapping("/log-test")
+    public String logTest() {
+        String name = "Spring";
+
+        log.trace("trace log={}", name);
+        log.debug("debug log={}", name);
+        log.info(" info log={}", name);
+        log.warn("warn log={}", name);
+        log.error("error log= {}", name);
+        return "ok";
+    }
+}
