32c3a4032507035e67bea16fd2e81c781a67177e
cjh8746 <cjh87467@gmail.com>
2022-04-12 16:04:11 +0900

    refactor : 체크 박스 멀티 추가
    지역별 체크박스 멀티를 생성함

--- 스프링 mvc2 /form/src/main/java/hello/itemservice/web/form/FormItemController.java
+++ 스프링 mvc2 /form/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -8,7 +8,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/form/items")
@@ -17,8 +19,18 @@ public class FormItemController {
 
     private final ItemRepository itemRepository;
 
+    @ModelAttribute("regions")
+    public Map<String, String> regions() {
+        Map<String, String> regions = new LinkedHashMap<>();
+        regions.put("SEOUL", "서울");
+        regions.put("SUSAN", "부산");
+        regions.put("JEJU", "제주");
+        return regions;
+    }
+
     @GetMapping
     public String items(Model model) {
+
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
         return "form/items";
