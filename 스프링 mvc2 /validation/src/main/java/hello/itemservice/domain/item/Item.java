a6d1d9928f3ae1b9f38da60a8accaeb79f847ffd
cjh8746 <cjh87467@gmail.com>
2022-04-22 16:37:33 +0900

    feat : Bean Validation 적용

--- 스프링 mvc2 /validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ 스프링 mvc2 /validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -8,6 +8,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity>=10000", message = "총합이 10000원 넘게 입력해주세요.")
 public class Item {
 
     private Long id;
@@ -20,7 +21,7 @@ public class Item {
     private Integer price;
 
     @NotNull
-    @Max(9999)
+//    @Max(9999)
     private Integer quantity;
 
     public Item() {
