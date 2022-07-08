98df07152ee7afc88ca0ae8aff7e57c7cb7f0b45
cjh8746 <cjh87467@gmail.com>
2022-07-08 14:41:10 +0900

    refactor  : AppConfig 생성
    DIP 를 지키기 위해 관심사를 분리하여, 구체 클래스를 주입 해주는 클래스인 AppConfig 구현
    및
    구현에 따른 수정

--- 스프링 기본 강의/core2/src/main/java/hello/core/OrderApp.java
+++ 스프링 기본 강의/core2/src/main/java/hello/core/OrderApp.java
@@ -2,18 +2,17 @@ package hello.core;
 
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
-import hello.core.order.OrderServiceImpl;
 
 import static hello.core.member.Grade.VIP;
 
 public class OrderApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", VIP);
