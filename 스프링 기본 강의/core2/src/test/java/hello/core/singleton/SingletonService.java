3c278e6348d6ecaaf0f6a06e2fef64adbaf97a5c
cjh8746 <cjh87467@gmail.com>
2022-07-13 17:24:17 +0900

    싱글톤 패턴을 확인하기 위해 싱글톤 객체를 만들고 테스트 함

---
+++ 스프링 기본 강의/core2/src/test/java/hello/core/singleton/SingletonService.java
@@ -0,0 +1,13 @@
+package hello.core.singleton;
+
+public class SingletonService {
+
+    public static final SingletonService instance = new SingletonService();
+
+    public static SingletonService getInstance() {
+        return instance;
+    }
+
+    private SingletonService() {
+    }
+}
