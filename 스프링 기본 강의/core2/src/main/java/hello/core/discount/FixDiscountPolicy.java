7bba9c3c55eb6e34d2c4fbef3bb48cd0e859865c
cjh8746 <cjh87467@gmail.com>
2022-07-14 16:15:17 +0900

    - 조회 빈이 2개 이상 - 문제
    @AutoWired 사용지 조뵈 빈이 2개 이상이라 중복이 되는 경우에 대한 에러 예시 코드
    -> FixDiscountPolicy 와 RateDiscountPolicy 이 중복됨

--- 스프링 기본 강의/core2/src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ 스프링 기본 강의/core2/src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -2,14 +2,16 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.stereotype.Component;
 
+@Component
 public class FixDiscountPolicy implements DiscountPolicy {
 
     private int discountFixAmount = 1000;
 
     @Override
     public int discount(Member member, int price) {
-        if(member.getGrade()== Grade.VIP) {
+        if (member.getGrade() == Grade.VIP) {
             return discountFixAmount;
         }
         return 0;
