66fe676733dc207f69094b3a14611a0a856d46fc
cjh8746 <cjh87467@gmail.com>
2022-07-18 19:07:15 +0900

    style  : 줄띄움 변경

--- 스프링 기본 강의/core2/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ 스프링 기본 강의/core2/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -17,8 +17,8 @@ class ConfigurationSingletonTest {
 
         MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
         OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
-        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
 
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
         MemberRepository memberRepository1 = memberService.getMemberRepository();
         MemberRepository memberRepository2 = orderService.getMemberRepository();
 
