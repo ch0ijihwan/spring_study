3e6a75afb1e2ff3801bd1b797699eaee33b6fb4e
cjh8746 <cjh87467@gmail.com>
2022-07-28 19:08:18 +0900

    RedirectAttributes

--- mvc 다시 듣기/1/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ mvc 다시 듣기/1/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -69,16 +70,24 @@ public class BasicItemController {
         return "/basic/item";
     }
 
-//    @PostMapping("/add")
+    //    @PostMapping("/add")
     public String addItemV4(Item item) {//@ModelAttribute 도 생략 가능
         itemRepository.save(item);
         return "/basic/item";
     }
 
-    @PostMapping("/add") // 리다이렉트 적용 PRG 패턴
+    //    @PostMapping("/add") // 리다이렉트 적용 PRG 패턴
     public String addItemV5(Item item) {
         itemRepository.save(item);
-        return "redirect:/basic/items/"+item.getId();
+        return "redirect:/basic/items/" + item.getId();
+    }
+
+    @PostMapping("/add") // 리다이렉트 적용 PRG 패턴
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}";
     }
 
     @GetMapping("/{itemId}/edit")
@@ -89,11 +98,10 @@ public class BasicItemController {
     }
 
     @PostMapping("/{itemId}/edit")
-    public String edit(@PathVariable Long itemId, @ModelAttribute Item item){
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
         itemRepository.update(itemId, item);
         return "redirect:/basic/items/{itemId}";
     }
-
 
 
     /**
