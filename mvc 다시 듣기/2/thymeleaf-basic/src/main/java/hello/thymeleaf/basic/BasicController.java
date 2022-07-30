35f67c5369125911e27e29a96b6e8ca0aa60cff1
cjh8746 <cjh87467@gmail.com>
2022-07-30 14:56:21 +0900

    - 연산

--- mvc 다시 듣기/2/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ mvc 다시 듣기/2/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -77,12 +77,17 @@ public class BasicController {
     }
 
     @GetMapping("/literal")
-    public String literal(Model model){
+    public String literal(Model model) {
         model.addAttribute("data", "Spring!");
         return "basic/literal";
     }
 
-
+    @GetMapping("/operation")
+    public String operation(Model model) {
+        model.addAttribute("nullData", null);
+        model.addAttribute("data", "Spring!");
+        return "basic/operation";
+    }
 
     @Data
     static class User {
