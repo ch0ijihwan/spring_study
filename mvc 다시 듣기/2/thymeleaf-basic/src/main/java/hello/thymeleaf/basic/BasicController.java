c61a8bb2aa6511b7952a9d90cc5145ec647cb4ef
cjh8746 <cjh87467@gmail.com>
2022-07-30 12:17:50 +0900

    유틸리티 객체와 날짜

--- mvc 다시 듣기/2/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ mvc 다시 듣기/2/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -50,16 +51,22 @@ public class BasicController {
     }
 
     @GetMapping("/basic-objects")
-    public String basicObjects(HttpSession session){
+    public String basicObjects(HttpSession session) {
         session.setAttribute("sessionData", "Hello Session");
         return "basic/basic-objects";
     }
 
     @Component("helloBean")
     static class HelloBean {
-        public  String hello(String data){
+        public String hello(String data) {
             return "Hello " + data;
         }
+    }
+
+    @GetMapping("/date")
+    public String date(Model model) {
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "basic/data";
     }
 
     @Data
